41923599d0122a90732490635490d75e4b75122b
simo255 <bmohamed255@gmail.com>
2020-10-26 18:19:01 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -11,13 +11,13 @@ def comparer(fichierr1 : str, fichierr2 : str):
     with open(fichierr1, 'r') as f, open(fichierr2,"r") as k:
         same = True
         while same:
-            a= fichietr1.read
-            b=fichier2.read
+            a= fichierr1.read
+            b=fichierr2.read
             same =  a==b
     return -1 if same else fichierr1.tell
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    comparer(fichier1.txt, fichier2.txt)
+    comparer(èfichier1.txt, fichier2.txt)
 
     pass
