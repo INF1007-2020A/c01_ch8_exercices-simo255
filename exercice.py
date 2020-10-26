ca4cd7ee857d47507844a722c5d5c465540fb121
simo255 <bmohamed255@gmail.com>
2020-10-26 18:12:38 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     with open(fichier1, 'r') as f, open(fichier2,"r") as k:
         same = True
         while same:
+            a= fichier1.read
+            b=fichier2.read
             
 
     pass
