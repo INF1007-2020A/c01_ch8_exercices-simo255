7bff3d288da1d37701e70cc27ea2df72ae8c3ca4
simo255 <bmohamed255@gmail.com>
2020-10-28 18:03:42 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,7 +24,8 @@ def spacetripler(f1):
 def notecalc(file1, file2):
     with open(file1, "r") as data, open(file2, "w") as result:
         for lignes in file1:
-            notes= files.readline()
+            notes= file1.readline()
+            
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     spacetripler('f1.txt')
