78afe4a8a585259192a11deb38193f19f03cfd1e
simo255 <bmohamed255@gmail.com>
2020-10-28 18:24:32 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,6 +31,7 @@ def assign_note_letter(note_file, target_file):
                     target.write(correspondances[grade] + "\n")
                     break
         
+def 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     assign_note_letter('notes.txt', "lol.txt")
