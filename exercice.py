11361a9ee37e9c913721e0fb608f73c0dc35ef88
simo255 <bmohamed255@gmail.com>
2020-10-28 18:26:58 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -34,7 +34,7 @@ def assign_note_letter(note_file, target_file):
 def numpicker¨():
     chiffres=[]
     with open('exemple.txt', 'r') as numbers:
-        for 
+        for nu
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     assign_note_letter('notes.txt', "lol.txt")
