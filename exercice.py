643c8c08b62d6acbd002687404657378ac4e05c3
simo255 <bmohamed255@gmail.com>
2020-10-28 17:59:26 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def spacetripler(f1):
     return result
 
 def notecalc(file1, file2):
-    with open(file1, "r")
+    with open(file1, "r") as data, open
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     spacetripler('f1.txt')
